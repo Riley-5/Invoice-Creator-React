7eda97785abad3f589f52780c8929b35a83d8791
Riley <riley.kamstra@gmail.com>
2022-05-16 11:49:06 +0200

    Header completed

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+/*
+    Props: service name and serive amount
+*/
+
+function Button(props) {
+    return (
+        <button></button>
+    )
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from "react"
 
 function Header() {
     return (
-        <>
-            <h1>Invoice creator</h1>
-            <p>Thanks for choosing GoodCorp, LLC!</p>
-        </>
+        <div id="header-container">
+            <h1 id="heading-el">Invoice creator</h1>
+            <p id="subtext-el">Thanks for choosing GoodCorp, LLC!</p>
+        </div>
     )
 }
 
