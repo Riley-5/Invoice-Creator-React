796b3f0ff1bb2771dd886ab343a1214de5fdfea6
Riley <riley.kamstra@gmail.com>
2022-05-16 11:35:33 +0200

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header() {
+    return (
+        <>
+            <h1>Invoice creator</h1>
+            <p>Thanks for choosing GoodCorp, LLC!</p>
+        </>
+    )
+}
+
+export default Header
