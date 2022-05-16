0cdd61dc3753a7efc199448d50f58334be44e38a
Riley <riley.kamstra@gmail.com>
2022-05-16 12:11:42 +0200

    Tasks component styled

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Tasks() {
+    return (
+        <div id="tasks-container">
+            <p>Tasks</p>
+            <p>Total</p>
+        </div>
+    )
+}
+
+export default Tasks
