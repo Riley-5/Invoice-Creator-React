203911d42dd45fbc7383ffb1ec0039cc2ea8d1a5
Riley <riley.kamstra@gmail.com>
2022-05-16 12:22:22 +0200

    Total component styled

---
+++ src/components/Total.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Total() {
+    return (
+        <div id="total-container">
+            <div>
+                <p>Notes</p>
+                <p>We accept cash, credit card, or PayPal</p>
+            </div>
+            <div>
+                <p>TOTAL AMOUNT</p>
+                <p id="total-amount-el">$0</p>
+            </div>
+        </div>
+    )
+}
+
+export default Total
